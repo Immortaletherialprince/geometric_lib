8c77507e8c8478b4695b527e96d00649784412c3
Egor <7777egormodanov@gmail.com>
2025-03-08 12:24:16 +0300

    done

--- triangle.py
+++ triangle.py
@@ -1,5 +1,7 @@
+import math
+
 def area(a, b, c):
-    return (a + b + c) / 2
+    return math.sqrt((a + b + c) / 2 * (b + c - a) / 2 * (a + c - b) / 2 * (a + b - c) / 2)
 
 
 def perimeter(a, b, c):
